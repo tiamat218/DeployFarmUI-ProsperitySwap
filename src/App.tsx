1ad748638efa4cf2825416c214c2ab6e89ff9ae4
tiamat218 <cebe.devrim@gmail.com>
2024-07-16 00:23:00 +0200

    ui changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, Suspense, lazy } from 'react'
+import styled from 'styled-components'
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
 import { useWallet } from '@binance-chain/bsc-use-wallet'
 import { ResetCSS } from '@pancakeswap-libs/uikit'
@@ -8,7 +9,6 @@ import GlobalStyle from './style/Global'
 import Menu from './components/Menu'
 import PageLoader from './components/PageLoader'
 
-
 // Route-based code splitting
 // Only pool is included in the main bundle because of it's the most visited page'
 const Home = lazy(() => import('./views/Home'))
@@ -16,13 +16,22 @@ const Farms = lazy(() => import('./views/Farms'))
 const Pools = lazy(() => import('./views/Pools'))
 const NotFound = lazy(() => import('./views/NotFound'))
 
-
-
 // This config is required for number formating
 BigNumber.config({
   EXPONENTIAL_AT: 1000,
   DECIMAL_PLACES: 80,
 })
+
+const Background = styled.div`
+  background-image: url('/images/egg/3b.png'); // oder den korrekten Pfad zu deinem Bild verwenden
+  background-size: contain;
+  background-position: right center;
+  background-repeat: no-repeat;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+`
 
 const App: React.FC = () => {
   const { account, connect } = useWallet()
@@ -38,26 +47,22 @@ const App: React.FC = () => {
     <Router>
       <ResetCSS />
       <GlobalStyle />
+      <Background />
       <Menu>
         <Suspense fallback={<PageLoader />}>
           <Switch>
-            
             <Route path="/" exact>
               <Home />
             </Route>
-
             <Route path="/farms">
               <Farms />
             </Route>
-   
             <Route path="/pools">
-              <Farms tokenMode/>
+              <Farms tokenMode />
             </Route>
-
             <Route path="/staking">
               <Pools />
             </Route>
-
             <Route component={NotFound} />
           </Switch>
         </Suspense>
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,51 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Heading, Text, BaseLayout, Button, BackgroundImage } from '@pancakeswap-libs/uikit'
+import { Heading, BaseLayout, Button } from '@pancakeswap-libs/uikit'
 import useI18n from 'hooks/useI18n'
 import Page from 'components/layout/Page'
 
-
-
 import FarmStakingCard from './components/FarmStakingCard'
-import FarmStakingCard1 from './components/FarmStakingCard1'
 import CakeStats from './components/CakeStats'
 import TotalValueLockedCard from './components/TotalValueLockedCard'
 import TwitterCard from './components/TwitterCard'
 import InfoCard from './components/InfoCard'
 
-// import Timer from './components/Timer'
-
-import TimerCard from './components/TimerCard'
 
 
 
-const Hero = styled.div`  // mobile verision
-  align-items: center;
-  background-image: url('/images/egg/3b.png');
-  background-repeat: no-repeat;
-
-  background-position: top center;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  margin: auto;
-  margin-top: -160px;
-  margin-bottom: -25px;
-  height: 325px;
-  padding-top: 1px;
-
-  ${({ theme }) => theme.mediaQueries.lg} {  // desktop version
-    background-image: url('/images/egg/3b.png');
-    background-size: cover;
-    background-position: center;
-    
-    
-    margin-top: -100px;
-    margin-bottom: -50px;
-    height: 500px;
-    
-    padding-top: 1;
-  }
+const Content = styled.div`
+  z-index: 1;
+  padding: 20px;
+  position: relative;
 `
 
 const Cards = styled(BaseLayout)`
@@ -75,56 +46,27 @@ const Home: React.FC = () => {
   const TranslateString = useI18n()
 
   return (
-
-
-
     <Page>
-
-
-
-    
-<br/>
-      <Hero>
-      <br/>
-      </Hero>
-
-      <Button 
-              as="a"
-              variant="primary"
-              href=""
-              target="_blank" >
-              Get GMT
+      <Content>
+        <Button 
+          as="a"
+          variant="primary"
+          href=""
+          target="_blank" >
+          Get GMT
         </Button>
 
-        <br/><br/>
-
-        <Heading as="h2"  color='#bdbdbd'> Advanced Sustainable Earning Technology </Heading>
-        <Heading as="h2"  color='#bdbdbd' mb="12px"> Blast Yield + Transaction fees for BuyBack! </Heading>
-        
-
-    
-
-      <div>
-
+        <Heading as="h2" color='#bdbdbd'>Advanced Sustainable Earning Technology</Heading>
+        <Heading as="h2" color='#bdbdbd' mb="12px">Blast Yield + Transaction fees for BuyBack!</Heading>
 
         <Cards>
           <FarmStakingCard />
-        {/*   <FarmStakingCard1 />
-
-
-          <TimerCard /> */}
-
-          <TwitterCard/>
-
+          <TwitterCard />
           <CakeStats />
-
-          <InfoCard/>         
-
+          <InfoCard />
           <TotalValueLockedCard />
-
         </Cards>
-        
-      </div>
+      </Content>
     </Page>
   )
 }
